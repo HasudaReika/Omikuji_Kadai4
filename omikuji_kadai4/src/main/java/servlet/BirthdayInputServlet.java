329367b55c1edc7d7f1450a897596e0b8d87a448
r_hasuda <r_hasuda@192.168.40.174>
2025-07-23 15:33:50 +0900

    エラーメッセージの表示とおみくじを続けるボタンの実装　cssが一部適応されないので修正中

--- omikuji_kadai4/src/main/java/servlet/BirthdayInputServlet.java
+++ omikuji_kadai4/src/main/java/servlet/BirthdayInputServlet.java
@@ -51,7 +51,7 @@ public class BirthdayInputServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		//エラーメッセージを設定
-		//		String errorMsg = (String) request.getAttribute("errorMsg");
+		String errorMsg = (String) request.getAttribute("errorMsg");
 
 		//HTMLを出力
 		response.setContentType("text/html; charset=UTF-8");
@@ -61,18 +61,22 @@ public class BirthdayInputServlet extends HttpServlet {
 		out.println("<head>");
 		out.println("<meta charset=\"UTF-8\"/>");
 		out.println("<title>おみくじ占い</title>");
+		out.println("<link rel=\"stylesheet\" href=\"" + request.getContextPath() + "/css/style.css\">");
 		out.println("</head>");
 		out.println("<body>");
+		out.println("<div class=\"container\">");
+		out.println("<h1>おみくじ占い</h1>");
 		out.println("<form action=\"result\" method=\"post\">");
 
-		//		if (errorMsg != null && errorMsg.isEmpty()) {
-		//			out.println("<p> class=\"errorMsg\">" errorMsg + "</p>");
-		//		}
+		if (errorMsg != null) { //エラーメッセージがある場合は表示
+			out.println("<p class=\"error-msg\">" + errorMsg + "</p>");
+		}
 
 		out.println("<p>誕生日を入力してください 例:20011009<br>");
-		out.println("<input type=\"text\" name=\"birthday\"/><br>");
+		out.println("<input type=\"text\" name=\"birthday\" required/><br>");
 		out.println("<input type=\"submit\" value=\"送信\"/></p>");
 		out.println("</form>");
+		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
 
@@ -88,6 +92,7 @@ public class BirthdayInputServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		//--------日付の妥当性チェック---------
+		//エラーメッセージの変数を準備
 		String errorMsg = null;
 		String birthday = (String) request.getParameter("birthday");
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
@@ -100,13 +105,18 @@ public class BirthdayInputServlet extends HttpServlet {
 		//入力された日付が正しいかチェック
 		try {
 			sdf.parse(birthday);
-			//正しかったらおみくじを出す
+			//正しければおみくじを取得する処理へ
 
 		} catch (ParseException e) {
 			//存在しない日付の場合、エラーメッセージを出力
 			errorMsg = "正しい日付を入力してください";
+			//リクエストスコープに値をセット
 			request.setAttribute("errorMsg", errorMsg);
-
+			//コンソールに出力用
+			System.out.println(errorMsg);
+			//エラーメッセージとともにフォームを再表示
+			doGet(request, response);
+			return;
 		}
 
 		//--------おみくじを取得する---------
@@ -119,27 +129,31 @@ public class BirthdayInputServlet extends HttpServlet {
 		String birthdayString = Normalizer.normalize(birthday, Form.NFKC);
 		LocalDate bdDate = LocalDate.parse(birthdayString, formatter);
 
+		//resultテーブルから取得するおみくじ変数
 		Omikuji omikuji = null;
+		//ランダムに新しいおみくじを取得する変数
 		Omikuji newOmikuji = null;
-		//resultテーブルに占った日と誕生日が一致する結果が存在するかチェック、あればおみくじを取得
-		//なければnullを返す
 
+		//resultテーブルに占った日と誕生日が一致する結果が存在するかチェック、あればおみくじを取得
 		try {
 			omikuji = omikujiDB.getOmikujiFromResult(today, bdDate);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 
-		if (omikuji != null) {
+		if (omikuji != null) { //該当するおみくじがある場合
 			//DBからおみくじを取得しリクエストスコープにセット
 			request.setAttribute("omikuji", omikuji);
+			//コンソールに出力用
 			System.out.println(omikuji.disp());
 
 		} else {
-			//DBからおみくじをランダムに1つ取得
+			//なければDBからおみくじをランダムに1つ取得
 			try {
 				newOmikuji = omikujiDB.getRandomOmikuji();
+				//リクエストスコープに値をセット
 				request.setAttribute("omikuji", newOmikuji);
+				//コンソールに出力用
 				System.out.println(newOmikuji.disp());
 			} catch (ClassNotFoundException | SQLException e) {
 				e.printStackTrace();
--- omikuji_kadai4/src/main/webapp/result.jsp
+++ omikuji_kadai4/src/main/webapp/result.jsp
@@ -10,16 +10,15 @@ Omikuji omikuji = (Omikuji) request.getAttribute("omikuji");
 <head>
 <meta charset="UTF-8">
 <title>おみくじ結果</title>
+<link rel="stylesheet" href="<%= request.getContextPath() %>/css/style.css">
 </head>
 <body>
-	<p><%=omikuji.disp()%></p><br>
-	<!-- <button onclick="confirmAction()">おみくじを続けますか？</button>
-	
-	<script>
-		function confirmAction(){
-			/* はいいいえボタン */
-			}
-	</script> -->
-	
+<div class="container">
+	<p><%=omikuji.disp().replace("\n", "<br>")%></p>
+	<p>おみくじを続けますか？</p>
+	<input type="button"
+		onclick="location.href='http://localhost:8080/omikuji_kadai4/result'"
+		value="続ける" />
+</div>
 </body>
 </html>
